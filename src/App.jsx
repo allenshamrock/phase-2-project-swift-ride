c133d79340744279895aecce2f25849a0c1c2150
Allanshikomere <allanshikomere@gmail.com>
2024-02-13 17:48:07 +0300

    Slider files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import VehicleSlider from './components/vehicle/VehicleSlider'; 
+
+const App = () => {
+  return (
+    <div className='vehicle-bazzar'>
+      <VehicleSlider />
+    </div>
+  );
+};
+
+export default App;
