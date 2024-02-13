34c5052f8023012655876a0cbfaf9a597ab56296
Allen <allenshamrock37@gmail.com>
2024-02-13 10:18:55 +0300

    Installation of environment dependancies

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import Applayout from "./Applayout";
 import { BrowserRouter } from "react-router-dom";
 function App() {
@@ -13,28 +12,3 @@ function App() {
 }
 
 export default App;
-=======
-import React from 'react'
-import UserAuth from './components/UserAuth'
-
-const App = () => {
-=======
-import Applayout from "./Applayout";
-import { BrowserRouter } from "react-router-dom";
-function App() {
->>>>>>> 4c2b601 (fixed merged conflicts)
-  return (
-    <>
-      <BrowserRouter>
-        <Applayout />
-      </BrowserRouter>
-    </>
-  );
-}
-
-<<<<<<< HEAD
-export default App
->>>>>>> dec1867 ( {Feat}:Implemented the user Auth)
-=======
-export default App;
->>>>>>> 4c2b601 (fixed merged conflicts)
