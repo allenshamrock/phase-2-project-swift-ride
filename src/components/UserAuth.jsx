9ab2047dc53e5fdf9a459829189e1a10b1a38aa9
Allen <allenshamrock37@gmail.com>
2024-02-13 20:38:58 +0300

    {Enhance}: Completed the userAuth compatment

--- src/components/UserAuth.jsx
+++ src/components/UserAuth.jsx
@@ -5,6 +5,10 @@ import {
 } from "../Schemas/index";
 import {
   Modal,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
   ModalOverlay,
   ModalContent,
   ModalHeader,
@@ -32,8 +36,18 @@ const UserAuth = () => {
     firstName: "",
     lastName: "",
     password: "",
+    confirmPassword:'',
   };
 
+  // Reset the form after successful submission
+  const handleFormSubmit = async (values, actions) => {
+    actions.resetForm({
+      values:
+        activeStatus === "signin" ? signinInitialValues : signupInitialValues,
+    });
+  };
+
+  
   function handleClickModal(clickedStatus) {
     console.log("clickedStatus");
     setActiveStatus(clickedStatus);
@@ -65,42 +79,138 @@ const UserAuth = () => {
                   ? signinValidationSchema
                   : signupValidationSchema
               }
+              onSubmit={handleFormSubmit}
             >
               {activeStatus === "signin" ? (
                 <Form>
                   <Stack direction="column" spacing={8}>
-                    <Field type="email" name="email" placeholder="Email" />
-                    <Field
-                      type="number"
-                      name="password"
-                      placeholder="Password"
-                    />
+                    <Field name="email">
+                      {({ field, form }) => (
+                        <FormControl
+                          isInvalid={form.errors.email && form.touched.email}
+                        >
+                          <FormLabel>Email </FormLabel>
+                          <Input {...field} type="type" id="email" />
+                          <FormErrorMessage>
+                            {form.errors.email &&
+                              form.touched.email &&
+                              form.errors.email}
+                          </FormErrorMessage>
+                        </FormControl>
+                      )}
+                    </Field>
+                    <Field name="password">
+                      {({ field, form }) => (
+                        <FormControl
+                          isInvalid={
+                            form.errors.password && form.touched.password
+                          }
+                        >
+                          <FormLabel>Password</FormLabel>
+                          <Input {...field} type="password" id="email" />
+                          <FormErrorMessage>
+                            {form.errors.password &&
+                              form.touched.password &&
+                              form.errors.password}
+                          </FormErrorMessage>
+                        </FormControl>
+                      )}
+                    </Field>
                   </Stack>
                 </Form>
               ) : (
                 <Form>
                   <Stack direction="column" spacing={8}>
-                    <Field
-                      type="text"
-                      name="firstName"
-                      placeholder="First name"
-                    />
-                    <Field
-                      type="text"
-                      name="lastName"
-                      placeholder="Last name"
-                    />
-                    <Field type="email" name="email" placeholder="Email" />
-                    <Field
-                      type="password"
-                      name="password"
-                      placeholder="Password"
-                    />
-                    <Field
-                      type="password"
-                      name="Confirm password"
-                      placeholder="Confirm Password"
-                    />
+                    <Field name="firstName">
+                      {({ field, form }) => (
+                        <FormControl
+                          isInvalid={
+                            form.errors.firstName && form.touched.firstName
+                          }
+                        >
+                          <FormLabel>First Name</FormLabel>
+                          <Input {...field} type="text" id="firstName" />
+                          <FormErrorMessage>
+                            {form.errors.firstName &&
+                              form.touched.firstName &&
+                              form.errors.firstName}
+                          </FormErrorMessage>
+                        </FormControl>
+                      )}
+                    </Field>
+                    <Field name="lastName">
+                      {({ field, form }) => (
+                        <FormControl
+                          isInvalid={
+                            form.errors.lastName && form.touched.lastName
+                          }
+                        >
+                          <FormLabel>Last Name</FormLabel>
+                          <Input {...field} type="text" />
+                          <FormErrorMessage>
+                            {form.errors.lastName &&
+                              form.touched.lastName &&
+                              form.errors.lastName}
+                          </FormErrorMessage>
+                        </FormControl>
+                      )}
+                    </Field>
+                    <Field name="password">
+                      {({ field, form }) => (
+                        <FormControl
+                          isInvalid={
+                            form.errors.password && form.touched.password
+                          }
+                        >
+                          <FormLabel>Password</FormLabel>
+                          <Input {...field} type="password" />
+                          <FormErrorMessage>
+                            {form.errors.password &&
+                              form.touched.password &&
+                              form.errors.password}
+                          </FormErrorMessage>
+                        </FormControl>
+                      )}
+                    </Field>
+                    <Field name="confirmPassword">
+                      {({ field, form }) => (
+                        <FormControl
+                          isInvalid={
+                            form.errors.confirmPassword &&
+                            form.touched.confirmPassword
+                          }
+                        >
+                          <FormLabel> Confirm Password</FormLabel>
+                          <Input {...field} type="password" />
+                          <FormErrorMessage>
+                            {form.errors.confirmPassword &&
+                              form.touched.confirmPassword &&
+                              form.errors.confirmPassword}
+                          </FormErrorMessage>
+                        </FormControl>
+                      )}
+                    </Field>
+                    <Stack direction="row" spacing={4}>
+                      {activeStatus === "signin" ? (
+                        <Button
+                          width="70%"
+                          colorScheme="teal"
+                          variant="outline"
+                        >
+                          Sign In
+                        </Button>
+                      ) : (
+                        <></>
+                      )}
+                      <Button
+                        width={activeStatus === "sigin" ? "30%" : "100%"}
+                        bg="#32bb78"
+                        variant="solid"
+                        type="submit"
+                      >
+                        Sign Up
+                      </Button>
+                    </Stack>
                   </Stack>
                 </Form>
               )}
@@ -124,7 +234,7 @@ const UserAuth = () => {
             Sign in
           </Button>
           <Button
-            bg="#2BD083"
+            bg="#32bb78"
             size="sm"
             onClick={() => handleClickModal("signup")}
           >
