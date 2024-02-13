09a6bdb8d69c9e6972a5290b9e6ca1a39fb4df9b
anneirungu <anneirungu07@gmail.com>
2024-02-13 22:13:36 +0300

    removed app.jsx content

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,1 @@
-import React from 'react';
-import './App.css'; // Assuming you have a CSS file for styling
-import DatePicker from './components/DatePicker';
-
-function App() {
-  return (
-    <div className="App">
-      <DatePicker />
-    </div>
-  );
-}
-
-export default App;
 
