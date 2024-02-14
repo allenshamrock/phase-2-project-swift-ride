12c6a312855a5958cc7da0fd738664403a56059a
lkbrian <lkbrian742@gmail.com>
2024-02-14 18:24:37 +0300

    responsive navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,75 @@
-import { Heading } from "@chakra-ui/react";
-import Navbar from "./Navbar";
+import React from "react";
 import UserAuth from "./UserAuth";
+import { useDisclosure } from "@chakra-ui/react";
+import {
+  Heading,
+  Flex,
+  Button,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+} from "@chakra-ui/react";
+import { CgMenuCheese } from "react-icons/cg";
+import Navbar from "./Navbar";
 
-function Header() {
+const Header = () => {
+  // const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef();
+
   return (
-    <header className="header">
-      <Heading size={"lg"} color={"#32bb78"}fontStyle={'italic'}>
+    <Flex
+      as="div"
+      align="center"
+      justifyContent="space-between"
+      wrap="wrap"
+      padding="6"
+      top={"0"}
+      position={"absolute"}
+      color="gray.900"
+      width={"100%"}
+    >
+      <Heading className="sr-only" color={"#32bb78"}>
         SwiftRides
       </Heading>
-      <Navbar />
-      <div className="controls">
-    <UserAuth/>
-      </div>
-    </header>
+    <Flex alignItems={"center"}>
+    <UserAuth />
+    <Flex display={{ base: "flex", lg: "none" }}>
+        <Button
+          ref={btnRef}
+          onClick={onOpen}
+          p={2}
+          color="gray.700"
+          _hover={{ bg: "teal.100" }}
+        >
+          <CgMenuCheese />
+        </Button>
+        <Drawer
+          isOpen={isOpen}
+          placement="right"
+          onClose={onClose}
+          finalFocusRef={btnRef}
+        >
+          <DrawerOverlay />
+          <DrawerContent>
+            <Navbar/>
+          </DrawerContent>
+        </Drawer>
+      </Flex>
+
+      <Flex
+        display={{ base: "none", lg: "flex" }}
+        align="center"
+        color="gray.900"
+        fontSize="sm"
+        fontWeight="semibold"
+      >
+        <Navbar />
+      </Flex>
+    </Flex>
+     
+    </Flex>
   );
-}
+};
 
 export default Header;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,41 @@
+// import React from 'react';
+import { Flex } from "@chakra-ui/react";
 import { NavLink } from "react-router-dom";
 
-function Navbar() { 
-    return ( 
-      <nav>         
-          <NavLink className='links' to={"/"}>Home</NavLink>
-          <NavLink className='links' to={"/About"}>About</NavLink>
-          <NavLink className='links' to={"/Contact"}>Contact</NavLink>
-          <NavLink className='links' to={"/Hire"}>Hire</NavLink>
-      </nav>
-    );
-  }
-  
-  export default Navbar;
+const Navbar = () => {
+  const links = [
+    { name: "Home", to: "/" },
+    { name: "About", to: "/About" },
+    { name: "Contact", to: "/Contact" },
+    { name: "Hire", to: "/Hire" },
+  ];
+
+  return (
+    <>
+    <Flex
+      bg={"#fff"}
+      display={{ base: "flex", lg: "flex" }}
+      width={{ base: "100%", lg: "auto" }}
+      flexDirection={{base:'column',lg:"row"}}
+      gap={12}
+      alignItems={"center"}
+      fontSize={'1.2rem'}
+    >
+      {links.map((link) => (
+        <NavLink
+        style={{padding:"7px 20px"}}
+          key={link.to}
+          to={link.to}
+          fontWeight="semibold"
+          color="gray.900"
+          _hover={{ color: "teal.500" }}
+        >
+          {link.name}
+        </NavLink>
+      ))}
+    </Flex>
+    </>
+  );
+};
+
+export default Navbar;
