d3fde94cc902444ee69c15ff0230d02f46958309
lkbrian <lkbrian742@gmail.com>
2024-02-14 10:39:51 +0300

    Committing changes before applying stash

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,42 @@
-import Header from "./Header"
+import { Box,  Heading, SimpleGrid, Text } from "@chakra-ui/layout";
+import Header from "./Header";
+import { Card} from "@chakra-ui/card";
 
 function Contact() {
   return (
     <>
-    <Header />
-    <div>Contact</div>
-    </>
-  )
+      <Header />
+      <Box p={'20px'} pt={'100px'}>
+
+      <Heading textAlign="center">Get In Touch</Heading>
+      <SimpleGrid
+        spacing={4}
+        minChildWidth={'300px'}
+      >
+        <Card p={{base:'20px'}} display="flex" alignItems="center"justifyContent="center">
+            <Heading size="md"> Physical Address</Heading>
+            <Text>
+              View a summary of all your customers over the last month.
+            </Text>
+        </Card>
+        <Card p={{base:'20px'}} display="flex" alignItems="center"justifyContent="center">
+            <Heading size="md"> Email</Heading>
+            <Text>
+              View a summary of all your customers over the last month.
+            </Text>
+        </Card>
+        <Card p={{base:'20px'}} display="flex" alignItems="center"justifyContent="center">
+            <Heading size="md"> Call/Whatsapp</Heading>
+            <Text>
+              View a summary of all your customers over the last month.
+            </Text>
+        </Card>
+      </SimpleGrid>
+ 
+      </Box>
+      
+         </>
+  );
 }
 
-export default Contact
+export default Contact;
