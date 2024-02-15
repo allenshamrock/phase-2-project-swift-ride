eb7fec86ddad8213001696ca23573c4c80b90995
lkbrian <lkbrian742@gmail.com>
2024-02-15 10:35:22 +0300

    {Feature}: added an offers endpoint

--- src/Applayout.jsx
+++ src/Applayout.jsx
@@ -1,14 +1,14 @@
-import Hero from "./components/Hero";
 import Hire from "./components/Hire";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 function Applayout() {
   return (
     <>
     <Routes>     
-        <Route path="/" element={<Hero/>} />
+        <Route path="/" element={<Home/>} />
         <Route path="/About" element={<About/>} />
         <Route path="/Contact" element={<Contact/>} />
         <Route path="/Hire" element={<Hire/>} />
